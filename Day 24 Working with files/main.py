e64364a9a934e93b4bd4e77bd8f5873b6adad0ec
Saiswarup Dash <129862110+Ssai08@users.noreply.github.com>
2026-08-05 09:47:38 +0530

    Deleted Wrong naming convention

--- Day 24 Working with files/main.py
+++
@@ -1,6 +0,0 @@
-with open("C:/Users/saisw/OneDrive/Desktop/my_file.txt") as file:
-    text = file.read()
-    print(text)
-
-# "C:\Users\saisw\OneDrive\Desktop\Computer Keyboard Shortcut keys .pdf"
-# "C:\Users\saisw\OneDrive\Desktop\my_file.txt"
--- day 19(Turtle Racing Game)/test.py
+++
@@ -1,8 +0,0 @@
-import turtle as t
-
-sc = t.Screen()
-tim = t.Turtle(shape="turtle")
-
-tim.goto(100,100)
-
-sc.exitonclick()
